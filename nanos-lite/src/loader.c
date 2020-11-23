d93df34e1f447cb682ff3804294176f862fb7a77
zzdkirov <871224178@qq.com>
2020-11-23 16:51:10 +0800

    temp pa3.2

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,28 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr elfheader; //excute file header, locate start address
+  Elf_Phdr prgmheader;  //program loader header, locate every segment
+  ramdisk_read(&elfheader,0,sizeof(Elf_Ehdr));
+
+  size_t phoffset=elfheader.e_phoff;
+
+  for(uint16_t i=0;i<elfheader.e_phnum;i++){
+    ramdisk_read(&prgmheader,phoffset,sizeof(Elf_Phdr));
+    if(prgmheader.p_type==PT_LOAD){
+      void *vaddr = prgmheader.p_vaddr;
+      ramdisk_read(vaddr,prgmheader.p_offset,prgmheader.p_memsz);
+      memset(vaddr,'\0',prgmheader.p_memsz-prgmheader.p_filesz);
+    }
+    phoffset+=sizeof(Elf_Phdr);
+  }
+
+
+  return elfheader.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,25 @@
 #include "common.h"
 #include "syscall.h"
 
+int sys_yield(){
+  _yield();
+  return 0;
+}
+void sys_exit(uintptr_t arg){
+  _halt(arg);
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
+  //printf("%d\n",a[0]);
   switch (a[0]) {
+    case SYS_yield: c->GPRx=sys_yield();break;
+    case SYS_exit: sys_exit(a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
