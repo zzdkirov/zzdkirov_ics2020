a045392280b0d4e1fc68f0499a4b68154ed5226e
zzdkirov <871224178@qq.com>
2020-11-24 15:36:14 +0800

    finish pa3.2, think that pblm is easy to solve in pa3.3:

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "syscall.h"
 
+
 int sys_yield(){
   _yield();
   return 0;
@@ -20,6 +21,7 @@ size_t sys_write(int fd,const void *buf,size_t count){
 }
 
 
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -32,6 +34,7 @@ _Context* do_syscall(_Context *c) {
     case SYS_yield: c->GPRx=sys_yield();break;
     case SYS_exit: sys_exit(a[1]);break;
     case SYS_write: c->GPRx=sys_write(a[1],(void*)a[2],a[3]);break;
+    case SYS_brk:c->GPRx=0;break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -62,7 +62,16 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write,fd,buf,count);
 }
 
+extern char _end;
+static intptr_t program_brk=(intptr_t)&_end;
 void *_sbrk(intptr_t increment) {
+
+  intptr_t old_brk=program_brk;
+  intptr_t now_brk=old_brk+increment;
+  if(_syscall_(SYS_brk,now_brk,0,0)==0){
+    program_brk=now_brk;
+    return (void*)old_brk;
+  }
   return (void *)-1;
 }
 
