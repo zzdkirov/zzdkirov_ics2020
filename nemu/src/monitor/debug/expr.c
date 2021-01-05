340718c627e77a13898fd495039dc4fb86d89f36
tracer-ics2019 <tracer@njuics.org>
2021-01-05 13:16:58 +0800

    >  compile U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:16:58 up 4 min,  1 user,  load average: 0.75, 0.54, 0.25 ff7145fad0e4b53e6bcb3f23b1ada24040abc754

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -66,10 +66,10 @@ void init_regex() {
 
 typedef struct token {
   int type;
-  char str[1024];
+  char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[1024] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
