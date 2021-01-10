15b343717e13af344cb47cd27447277de91eb984
tracer-ics2019 <tracer@njuics.org>
2021-01-10 16:33:17 +0800

    >  run U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:33:17 up 24 min,  1 user,  load average: 0.83, 0.55, 0.34 9b85490fd972c689407ebbdd3a80d823e3dfed94

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -47,8 +47,9 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
   }
 
   set_cr3(kpdirs);
-  printf("fuck\n");
+  
   set_cr0(get_cr0() | CR0_PG);
+  printf("fuck\n");
   vme_enable = 1;
 
   return 0;
