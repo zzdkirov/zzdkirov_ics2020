581ad3a21a3dfe9eef9a8e7e4545a000fc57e491
tracer-ics2019 <tracer@njuics.org>
2021-01-10 16:58:24 +0800

    >  compile U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:58:24 up 49 min,  1 user,  load average: 0.99, 0.71, 0.55 fce4fdb0c86105501629492caa5afbf43231b64c

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -25,7 +25,6 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
   cr0.val=cpu.cr0;
   if(cr0.paging){
     if((addr & PAGE_MASK) + len > PAGE_SIZE){
-      printf("1\n");
       int len1,len2;
       paddr_t paddr1,paddr2;
       uint32_t val1,val2;
@@ -39,8 +38,6 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
       return (val2<<(len1<<3))|val1; 
     }
     else{
-      printf("2\n");
-      printf("%x\n%x\n",cpu.pc,cr0.paging);
       paddr_t paddr = page_translate(addr);
       return paddr_read(paddr, len);
     }
@@ -56,7 +53,6 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
   cr0.val=cpu.cr0;
   if(cr0.paging){
     if((addr & PAGE_MASK) + len > PAGE_SIZE){
-      printf("3\n");
       int len1,len2;
       paddr_t paddr1,paddr2;
       len1=PAGE_SIZE-(addr & PAGE_MASK);
@@ -66,7 +62,6 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
       paddr_write(paddr1,data,len1);
       paddr_write(paddr2,data>>(len1<<3),len2);
     }else{
-      printf("4\n");
       paddr_t paddr = page_translate(addr);
       paddr_write(paddr, data, len);
     }
