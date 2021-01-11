7b84087288a8bd79fc265da868137bf232aa3c32
tracer-ics2019 <tracer@njuics.org>
2021-01-11 18:45:48 +0800

    >  compile U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:45:48 up  2:56,  1 user,  load average: 0.90, 0.45, 0.27 4b47ed95c130186f491dead2d784e3453db1b5e

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -26,7 +26,8 @@ make_EHelper(mov_r2cr) {
     default:  //should not reach here
       assert(0);
   }
-
+  if(cpu.pc==0x101103)
+    printf("fuck\n");
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
