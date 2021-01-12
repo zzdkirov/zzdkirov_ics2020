334950b75e4544690dda33b380c60dc5fcab4861
tracer-ics2019 <tracer@njuics.org>
2021-01-12 12:25:37 +0800

    >  run U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:25:37 up 23 min,  1 user,  load average: 0.74, 0.35, 0.24 fb76739e88863296ab64284f1ee6bfd0d5a4e93a

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -32,12 +32,12 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
     }
     current->max_brk = brk;
   }
-/*
+
   while(current->max_brk<=brk){
     void *pa = new_page(1);
     _map(&current->as,(void*)current->max_brk,pa,0);
     current->max_brk+=PGSIZE;
-  }*/
+  }
   return 0;
 }
 
