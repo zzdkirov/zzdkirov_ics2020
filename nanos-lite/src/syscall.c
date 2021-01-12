981881180bdeff9ef7034402d8733132870b476f
tracer-ics2019 <tracer@njuics.org>
2021-01-12 13:29:51 +0800

    >  run U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:29:51 up  1:27,  1 user,  load average: 0.41, 0.21, 0.21 e6c8bdb03ce5434c5cf01c0fc99eed0f6364e365

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -51,7 +51,7 @@ _Context* do_syscall(_Context *c) {
     case SYS_close: c->GPRx=fs_close(a[1]);break;
     case SYS_open: c->GPRx=fs_open((char*)a[1],a[2],a[3]);break;
     case SYS_lseek: c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
-    case SYS_brk:c->GPRx=0;break;
+    case SYS_brk:c->GPRx=mm_brk(a[1],0);break;
     case SYS_execve:naive_uload(NULL,(char*)a[1]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
