4aded0e25feee691db9ea879649a3fcc04d54db8
tracer-ics2019 <tracer@njuics.org>
2021-01-12 12:25:13 +0800

    >  run U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:25:13 up 23 min,  1 user,  load average: 0.34, 0.24, 0.20 1ac2c7b81a591fc7bc3324992e627bc3a85c5280

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,6 +1,9 @@
 #include "memory.h"
+#include "proc.h"
 
 static void *pf = NULL;
+int _map(_AddressSpace *as, void *va, void *pa, int prot);
+extern PCB *current;
 
 void* new_page(size_t nr_page) {
   void *p = pf;
@@ -15,7 +18,26 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk, intptr_t increment) {
-  
+  //这个函数的实现我实在是不知道该怎么办了，所以就借鉴了网上的实现方法
+  //大意应该是brk找到它的最大值，如果比新分配的brk比它还大，就从最大值开始，每一个page都映射
+  //
+  if(current->max_brk<brk){
+    void *va = NULL,*pa = NULL;
+    void *begin =(void *) PGROUNDUP(current->max_brk);
+    void *end = (void*)PGROUNDUP(brk);
+    for(void*i = begin;i<=end;i+=PGSIZE){
+      va=(void*)i;
+      pa = new_page(1);
+      _map(&current->as,va,pa,0);
+    }
+    current->max_brk = brk;
+  }
+/*
+  while(current->max_brk<=brk){
+    void *pa = new_page(1);
+    _map(&current->as,(void*)current->max_brk,pa,0);
+    current->max_brk+=PGSIZE;
+  }*/
   return 0;
 }
 
