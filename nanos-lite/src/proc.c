5ad047673f51a6266e1aff3a3f7f2bbcc2a526e3
tracer-ics2019 <tracer@njuics.org>
2021-01-12 15:07:24 +0800

    >  run U201714510 zzdkirov Linux zzdkirov 5.4.0-58-generic #64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:07:24 up  3:05,  1 user,  load average: 0.61, 0.70, 0.59 6869050593335bbf151532283952fb82bfff31bb

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,8 +23,8 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_uload(&pcb[0],"/bin/pal");
-  context_uload(&pcb[1],"/bin/pal");
+  context_uload(&pcb[0],"/bin/init");
+  context_uload(&pcb[1],"/bin/hello");
   switch_boot_pcb();
   Log("Initializing processes...");
 
@@ -35,7 +35,7 @@ void init_proc() {
 int counter_schedule=0;
 _Context* schedule(_Context *prev) {
   current->cp=prev;
-  /*if(counter_schedule++ >0){
+  if(counter_schedule++ >0){
     current=&pcb[1];
     counter_schedule=0;
   }
@@ -43,7 +43,7 @@ _Context* schedule(_Context *prev) {
   {
     current=&pcb[0];
   }
-  */
-  current= (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  
+  //current= (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
