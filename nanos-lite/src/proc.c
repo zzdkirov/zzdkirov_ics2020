5b5143565534d3262022eb244ba3bab83daf529c
tracer-ics2019 <tracer@njuics.org>
2021-01-13 18:18:10 +0800

    >  run U201714510 zzdkirov Linux zzdkirov 5.8.0-36-generic #40~20.04.1-Ubuntu SMP Wed Jan 6 10:15:55 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:18:10 up  7:41,  1 user,  load average: 0.32, 0.52, 0.43 c59e97db89085143384f34e838a1b273d1bead70

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -52,7 +52,7 @@ void init_proc() {
 int counter_schedule=0;
 _Context* schedule(_Context *prev) {
   current->cp=prev;
-  if(counter_schedule++ >200){
+  if(counter_schedule++ >1){
     current=&pcb[0];
     counter_schedule=0;
   }
